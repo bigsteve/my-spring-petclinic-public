2bd965f9873631add30d1c72f48b9ce005df609a
unknown <usstefanmiller@yahoo.com>
2021-12-12 18:32:18 -0600

    commit again

---
+++ pet-clinic-web/src/test/java/stefan/spring/petclinicweb/test/PetClinicApplicatonTest.java
@@ -0,0 +1,16 @@
+package stefan.spring.petclinicweb.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetClinicApplicatonTest {
+
+//    @Test
+//    public void contextLoads() {
+//        assertEquals(0, 0);
+//    }
+
+}
